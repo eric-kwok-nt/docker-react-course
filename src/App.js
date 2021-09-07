57c4a23876706ec06430b2e90e438c1ff3090685
Eric_kwok <kwokngaitung777@gmail.com>
2021-09-07 14:18:42 +0800

    changed app text and .travis.yml

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. I will change in the Feature branch
+          Hello, Mona!! What are you up to?
         </p>
         <a
           className="App-link"
